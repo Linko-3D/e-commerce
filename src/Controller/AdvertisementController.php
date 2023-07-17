095a44e3f4d455f74dc089034a03a4875c1fb8e4
Linko-3D <dbekhouc@gmail.com>
2023-07-17 22:54:07 +0200

    tweak

--- src/Controller/AdvertisementController.php
+++ src/Controller/AdvertisementController.php
@@ -5,12 +5,17 @@ namespace App\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use App\Entity\Advertisement;
 
 class AdvertisementController extends AbstractController
 {
     #[Route('/', name: 'home')]
     public function index(): Response
     {
+
+        // $repo = $this->getDoctrine()->getRepository(Advertisement::class);
+        // $article = $repo->findAll();
+
         $nom = "danyl";
 
         return $this->render('home/index.html.twig', [
